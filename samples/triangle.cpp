3ab8b35905edc38bc637fd5ff0a552cebe0779f4
Joshua Reibert <joshua.reibert@uni-weimar.de>
2020-12-21 20:03:45 +0100

    Draw triangle

--- samples/triangle.cpp
+++ samples/triangle.cpp
@@ -1,5 +1,14 @@
+#include "buffer.hpp"
 #include "opengl.hpp"
+#include "program.hpp"
+#include "shader.hpp"
+#include "vertex_array.hpp"
 #include "window.hpp"
+
+struct Vertex {
+  glm::vec2 pos;
+  glm::vec4 color;
+};
 
 class TriangleWindow : public broomstick::Window {
  public:
@@ -30,12 +39,40 @@ int main(int argc, char const* argv[]) {
     return EXIT_FAILURE;
   }
 
-  while (!window.should_close()) {
-    glViewport(0, 0, window.resolution().x, window.resolution().y);
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    window.swap_buffers();
-    glfwPollEvents();
+  {
+    auto vert_shader = broomstick::Shader{GL_VERTEX_SHADER};
+    vert_shader.load_source_from_file("shaders/simple_color.vert");
+    auto frag_shader = broomstick::Shader{GL_FRAGMENT_SHADER};
+    frag_shader.load_source_from_file("shaders/simple_color.frag");
+    auto program = broomstick::Program{{vert_shader, frag_shader}};
+
+    auto vbo = broomstick::Buffer{};
+    const std::vector<Vertex> vertices = {
+        {glm::vec2{-0.5, -0.5}, glm::vec4{1.0f, 0.0f, 0.0f, 1.0f}},
+        {glm::vec2{0.5, -0.5}, glm::vec4{0.0f, 1.0f, 0.0f, 1.0f}},
+        {glm::vec2{0.0, 0.5}, glm::vec4{0.0f, 0.0f, 1.0f, 1.0f}},
+    };
+    vbo.set_data(sizeof(Vertex) * vertices.size(), vertices.data());
+
+    auto vao = broomstick::VertexArray{};
+    vao.set_vertex_buffer(0, vbo, 0, sizeof(Vertex));
+    vao.set_attribute_enabled(0, true);
+    vao.set_attribute_enabled(1, true);
+    vao.set_attribute_format(0, 2, GL_FLOAT, false, offsetof(Vertex, pos));
+    vao.set_attribute_format(1, 4, GL_FLOAT, false, offsetof(Vertex, color));
+    vao.set_attribute_binding(0, 0);
+    vao.set_attribute_binding(1, 0);
+
+    while (!window.should_close()) {
+      glViewport(0, 0, window.resolution().x, window.resolution().y);
+      glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+      program.use();
+      vao.bind();
+      glDrawArrays(GL_TRIANGLES, 0, 3);
+      window.swap_buffers();
+      glfwPollEvents();
+    }
   }
 
   glfwTerminate();
