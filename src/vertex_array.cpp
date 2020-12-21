b0692771b7f4a9c51109ce66246bce6ec6fd14e8
Joshua Reibert <joshua.reibert@uni-weimar.de>
2020-12-21 20:03:10 +0100

    Add vertex array class

---
+++ src/vertex_array.cpp
@@ -0,0 +1,83 @@
+#include "vertex_array.hpp"
+
+namespace broomstick {
+
+VertexArray::VertexArray() {
+  glCreateVertexArrays(1, &_id);
+}
+
+VertexArray::~VertexArray() {
+  destroy();
+}
+
+bool operator<(const VertexArray& lhs, const VertexArray& rhs) {
+  return lhs._id < rhs._id;
+}
+
+bool VertexArray::valid() const {
+  return glIsVertexArray(_id) != GL_FALSE;
+}
+
+GLuint VertexArray::id() const {
+  return _id;
+}
+
+void VertexArray::bind() const {
+  glBindVertexArray(_id);
+}
+
+void VertexArray::unbind() {
+  glBindVertexArray(0);
+}
+
+void VertexArray::set_element_buffer(const Buffer& buffer) {
+  glVertexArrayElementBuffer(_id, buffer.id());
+}
+
+void VertexArray::set_vertex_buffer(GLuint binding_index, const Buffer& buffer, GLintptr offset, GLsizei stride) {
+  glVertexArrayVertexBuffer(_id, binding_index, buffer.id(), offset, stride);
+}
+
+void VertexArray::set_attribute_enabled(GLuint index, bool enabled) {
+  if (enabled) {
+    glEnableVertexArrayAttrib(_id, index);
+  } else {
+    glDisableVertexArrayAttrib(_id, index);
+  }
+}
+
+void VertexArray::set_attribute_binding(GLuint index, GLuint binding_index) {
+  glVertexArrayAttribBinding(_id, index, binding_index);
+}
+
+void VertexArray::set_attribute_format(GLuint index, GLint size, GLenum type, bool normalized, GLuint relative_offset) {
+  glVertexArrayAttribFormat(_id, index, size, type, normalized, relative_offset);
+}
+
+void VertexArray::set_attribute_format_integer(GLuint index, GLint size, GLenum type, GLuint relative_offset) {
+  glVertexArrayAttribIFormat(_id, index, size, type, relative_offset);
+}
+
+void VertexArray::set_attribute_format_long(GLuint index, GLint size, GLenum type, GLuint relative_offset) {
+  glVertexArrayAttribLFormat(_id, index, size, type, relative_offset);
+}
+
+GLint VertexArray::get_parameter(GLenum parameter) const {
+  GLint result;
+  glGetVertexArrayiv(_id, parameter, &result);
+  return result;
+}
+
+GLint VertexArray::get_attribute_parameter(GLuint index, GLenum parameter) const {
+  GLint result;
+  glGetVertexArrayIndexediv(_id, index, parameter, &result);
+  return result;
+}
+
+void VertexArray::destroy() const {
+  if (valid()) {
+    glDeleteVertexArrays(1, &_id);
+  }
+}
+
+}  // namespace broomstick
---
+++ src/vertex_array.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <string>
+
+#include <glad/glad.h>
+#include <spdlog/spdlog.h>
+
+#include "buffer.hpp"
+
+namespace broomstick {
+
+class VertexArray {
+ public:
+  VertexArray();
+  VertexArray(const VertexArray&) = delete;
+  VertexArray(VertexArray&&) = default;
+  ~VertexArray();
+
+  VertexArray& operator=(const VertexArray& other) = delete;
+  VertexArray& operator=(VertexArray&& other) = default;
+
+  friend bool operator<(const VertexArray& lhs, const VertexArray& rhs);
+
+  bool valid() const;
+  GLuint id() const;
+
+  void bind() const;
+  static void unbind();
+
+  void set_element_buffer(const Buffer& buffer);
+  void set_vertex_buffer(GLuint binding_index, const Buffer& buffer, GLintptr offset = 0, GLsizei stride = 1);
+  void set_attribute_enabled(GLuint index, bool enabled = true);
+  void set_attribute_binding(GLuint index, GLuint binding_index);
+  void set_attribute_format(GLuint index, GLint size, GLenum type, bool normalized = false, GLuint relative_offset = 0);
+  void set_attribute_format_integer(GLuint index, GLint size, GLenum type, GLuint relative_offset = 0);
+  void set_attribute_format_long(GLuint index, GLint size, GLenum type, GLuint relative_offset = 0);
+
+ protected:
+  GLint get_parameter(GLenum parameter) const;
+  GLint get_attribute_parameter(GLuint index, GLenum parameter) const;
+  void destroy() const;
+
+ protected:
+  GLuint _id;
+};
+
+}  // namespace broomstick
